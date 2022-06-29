b8ecbd72bd2317b9246439bbb4bd435d2494b5bd
Irving J Villanueva Reyes <Irving.Villanueva@jala-foundation.org>
2022-06-29 14:08:55 -0500

    Added base project, folder organization and example page

---
+++ Pokedex_ios/View/Controller/PokemonDetailViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Pokedex_ios/View/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex_ios
+//
+//  Created by admin on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
