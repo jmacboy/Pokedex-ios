60e3a855ed10aa0cf08cb8ebbc43c848b22397e2
YawarValeriano <yawar.valeriano@jala-foundation>
2022-07-08 16:50:13 -0500

    Added base cell for filter options, needs constraint fixes

--- Pokedex_ios/View/Controller/AdvancedFilterPopup/AdvancedFilterPopupViewController.swift
+++ Pokedex_ios/View/Controller/AdvancedFilterPopup/AdvancedFilterPopupViewController.swift
@@ -12,23 +12,34 @@ class AdvancedFilterPopupViewController: PopupViewController {
 
     @IBOutlet weak var applyButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
-    
+    @IBOutlet weak var weaknessesCollectionView: UICollectionView!
+
+    var selectedTypesForWeakness = [TypeElement]()
+
     lazy var viewmodel = {
         AdvancedFilterPopupViewModel()
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         initViewModel()
         setupContent()
+        setupElements()
     }
-    
+
     func initViewModel() {
         viewmodel.closePopup = {[weak self] in
             self?.animateDismissView()
         }
     }
-    
+
+    func setupElements() {
+        let uiNibPokemonTypeCell = UINib(nibName: PokemonTypeCollectionCell.nibName, bundle: nil)
+        weaknessesCollectionView.register(uiNibPokemonTypeCell, forCellWithReuseIdentifier: PokemonTypeCollectionCell.identifier)
+        weaknessesCollectionView.delegate = self
+        weaknessesCollectionView.dataSource = self
+    }
+
     func setupContent() {
         applyButton.layer.cornerRadius = 10
         resetButton.layer.cornerRadius = 10
@@ -43,12 +54,39 @@ class AdvancedFilterPopupViewController: PopupViewController {
             contentStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20)
         ])
     }
-    
-    
+
     @IBAction func applyFilters(_ sender: Any) {
         viewmodel.applyFilters()
     }
     @IBAction func resetFilters(_ sender: Any) {
         viewmodel.resetFilters()
     }
+}
+
+extension AdvancedFilterPopupViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ConstantVariables.pokemonTypes.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let pokemonType = ConstantVariables.pokemonTypes[indexPath.row]
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PokemonTypeCollectionCell.identifier, for: indexPath)
+                as? PokemonTypeCollectionCell else { return UICollectionViewCell() }
+
+        let index = selectedTypesForWeakness.firstIndex(where: { $0.type.id == pokemonType.type.id })
+        cell.setupData(pokemonType: pokemonType, isTypeSelected: index != nil)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let pokemonType = ConstantVariables.pokemonTypes[indexPath.row]
+
+        if let index = selectedTypesForWeakness.firstIndex(where: { $0.type.id == pokemonType.type.id }) {
+            selectedTypesForWeakness.remove(at: index)
+        } else {
+            selectedTypesForWeakness.append(pokemonType)
+        }
+        weaknessesCollectionView.reloadItems(at: [indexPath])
+    }
+
 }
