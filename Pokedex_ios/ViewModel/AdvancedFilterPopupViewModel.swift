228ae8cbd89b4e2d7a352b144484b6a8889b1a05
YawarValeriano <yawar.valeriano@jala-foundation>
2022-07-11 18:02:55 -0500

    Fixed communication between viewModels

--- Pokedex_ios/ViewModel/AdvancedFilterPopupViewModel.swift
+++ Pokedex_ios/ViewModel/AdvancedFilterPopupViewModel.swift
@@ -7,13 +7,38 @@
 
 import Foundation
 
+protocol PokemonListViewModelDelegate {
+    func filteredPokemonData(filteredPokemons: [PokemonRaw])
+}
+
 class AdvancedFilterPopupViewModel {
+
+    var pokemonOriginal: [PokemonRaw]
+    var filtered = [PokemonRaw]()
+
+    var delegate: PokemonListViewModelDelegate?
+
+    init(pokemons: [PokemonRaw]) {
+        self.pokemonOriginal = pokemons
+    }
+
     var closePopup: (() -> Void)?
-    @objc func applyFilters() {
-        // TODO: Make all the filter logic here
+
+    func applyFilters(weaknesses: [TypeElement]) {
+        filterByWeaknesses(weaknesses)
+        self.delegate?.filteredPokemonData(filteredPokemons: filtered)
         closePopup?()
     }
+
     func resetFilters() {
         // TODO: Make all the logic for reset the filters
+    }
+
+    func filterByWeaknesses(_ weaknessesTypes: [TypeElement]) {
+        filtered = pokemonOriginal.filter { $0.pokemonDetails[0].weaknesses!.contains { weakness in
+            let aux = weaknessesTypes.contains { $0.type.id == weakness.fromType.id }
+            return weakness.damageTaken == 2.0 && aux
+        }}
+
     }
 }
