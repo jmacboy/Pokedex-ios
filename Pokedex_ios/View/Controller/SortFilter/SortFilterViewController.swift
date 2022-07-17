11625dfbbb9aab8a5b6297bbbb00bf3623300dca
adCBueno <adrianacc.bueno@gmail.com>
2022-07-17 12:38:11 -0500

    Pokemon sorting feature added

---
+++ Pokedex_ios/View/Controller/SortFilter/SortFilterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SortFilterViewController.swift
+//  Pokedex_ios
+//
+//  Created by admin on 7/15/22.
+//
+
+import UIKit
+
+class SortFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
